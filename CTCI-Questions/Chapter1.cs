3273ff0aec735e42446429644e5dc7a363d7b15f
Unknown <felix@felixsoum.com>
2020-01-29 21:57:14 -0500

    Add solutions to Q1.1-1.4

---
+++ CTCI-Questions/Chapter1.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+static class Chapter1
+{
+    // Is Unique
+    public static bool Question1(string input)
+    {
+        var set = new HashSet<char>();
+        foreach (var character in input)
+        {
+            if (set.Contains(character))
+            {
+                return false;
+            }
+            set.Add(character);
+        }
+        return true;
+    }
+
+    // Check Permutation
+    public static bool Question2(string input1, string input2)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach (var character in input1)
+        {
+            if (dict.ContainsKey(character))
+            {
+                dict[character]++;
+            }
+            else
+            {
+                dict.Add(character, 1);
+            }
+        }
+
+        foreach (var character in input2)
+        {
+            if (dict.ContainsKey(character))
+            {
+                if (--dict[character] == 0)
+                {
+                    dict.Remove(character);
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return dict.Count == 0;
+    }
+
+    // URLify
+    public static string Question3(string input)
+    {
+        // one liner
+        //return input.Replace(" ", "%20");
+
+        var builder = new StringBuilder();
+        foreach (var character in input)
+        {
+            builder.Append(character == ' ' ? "%20" : character.ToString());
+        }
+
+        return builder.ToString();
+    }
+
+    // Palindrome Permutation
+    public static bool Question4(string input)
+    {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var character in input)
+        {
+            if (dict.ContainsKey(character))
+            {
+                dict[character]++;
+            }
+            else
+            {
+                dict.Add(character, 1);
+            }
+        }
+
+        bool foundOddCharacter = false;
+        foreach (var count in dict.Values)
+        {
+            if (count % 2 == 1)
+            {
+                if (foundOddCharacter)
+                {
+                    return false;
+                }
+                else
+                {
+                    foundOddCharacter = true;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ CTCI-Questions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TestChapter1();
+    }
+
+    private static void TestChapter1()
+    {
+        Console.WriteLine("Question 1");
+        Console.WriteLine(Chapter1.Question1("abc"));
+        Console.WriteLine(Chapter1.Question1("abbc"));
+
+        Console.WriteLine("Question 2");
+        Console.WriteLine(Chapter1.Question2("abc", "bca"));
+        Console.WriteLine(Chapter1.Question2("abc", "bda"));
+
+        Console.WriteLine("Question 3");
+        Console.WriteLine(Chapter1.Question3("a b c"));
+
+        Console.WriteLine("Question 4");
+        Console.WriteLine(Chapter1.Question4("ttaacoc"));
+        Console.WriteLine(Chapter1.Question4("ttadacoc"));
+    }
+}
